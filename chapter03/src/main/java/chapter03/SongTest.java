8d4304f693191d9a11417eaddb4360bb36e4ea8a
hyojining <gywls1390@gmail.com>
2023-08-10 09:12:41 +0900

    Song

---
+++ chapter03/src/main/java/chapter03/SongTest.java
@@ -0,0 +1,17 @@
+package chapter03;
+
+public class SongTest {
+
+	public static void main(String[] args) {
+		Song song1 = new Song();
+		song1.setTitle("좋은날");
+		song1.setArtist("아이유");
+		song1.setAlbum("Real");
+		song1.setComposer("이민수");
+		song1.setTrack(3);
+		song1.setYear(2010);
+		
+		song1.show();
+	}
+
+}
