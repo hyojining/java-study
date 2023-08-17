bb36067af85df7e8984278176bc07a968e7e3452
hyojining <gywls1390@gmail.com>
2023-08-17 17:13:09 +0900

    NSLookup

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -1,10 +1,33 @@
 package util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class NSLookup {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print(">> ");
+			String line = scanner.nextLine();
+			
+			if("exit".equals(line)) {
+				break;
+			}
+			
+			InetAddress[] addresses;
+			try {
+				addresses = InetAddress.getAllByName(line);
+				for(InetAddress address : addresses) {
+					System.out.println(address.getHostName() + " : " + address.getHostAddress());
+				}
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+			
+		}
 	}
 
 }
